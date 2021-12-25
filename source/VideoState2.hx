2d4d7c1253e77317867742e6cffe25270765539c
Skeebyyy <95153655+Skeebyyy@users.noreply.github.com>
2021-12-24 22:00:59 -0300

    Update VideoState2.hx

--- source/VideoState2.hx
+++ source/VideoState2.hx
@@ -48,7 +48,6 @@ class VideoState2 extends MusicBeatState
 		
 		leSource = source;
 		transFunction = toTrans;
-		#if !desktop
 		if (frameSkipLimit != -1 && GlobalVideo.isWebm)
 		{
 			//GlobalVideo.getWebm().webm.SKIP_STEP_LIMIT = frameSkipLimit;	

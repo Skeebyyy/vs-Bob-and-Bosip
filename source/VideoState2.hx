302bd5dbbb0ba5f17ccaca2ffec49110fb87e4b0
Skeebyyy <95153655+Skeebyyy@users.noreply.github.com>
2021-12-24 22:43:03 -0300

    Create VideoState2.hx

--- source/VideoState2.hx
+++ source/VideoState2.hx
@@ -52,7 +52,8 @@ class VideoState2 extends MusicBeatState
 		{
 			//GlobalVideo.getWebm().webm.SKIP_STEP_LIMIT = frameSkipLimit;	
 			WebmPlayer.SKIP_STEP_LIMIT = frameSkipLimit;
-		#end
+		}
+	}
 	
 	override function create()
 	{

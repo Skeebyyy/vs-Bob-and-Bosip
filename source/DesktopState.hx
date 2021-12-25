c648b665eec13cacc79ac8717fd4662bbfe70b4c
Skeebyyy <95153655+Skeebyyy@users.noreply.github.com>
2021-12-24 21:12:35 -0300

    Update DesktopState.hx

--- source/DesktopState.hx
+++ source/DesktopState.hx
@@ -29,7 +29,6 @@ import flixel.tweens.FlxEase;
 import flixel.tweens.FlxTween;
 import flixel.util.FlxColor;
 import flixel.util.FlxTimer;
-import io.newgrounds.NG;
 import flixel.util.FlxSpriteUtil;
 import lime.app.Application;
 import openfl.Assets;
